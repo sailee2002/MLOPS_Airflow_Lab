4a9096c4a1195fc796d1dd89375e6d938272dbc7
Sailee <saichoudhari22@gmail.com>
2025-11-14 01:40:33 -0500

    Initial commit: Add Airflow Lab 1 with EDA task

---
+++ dags/airflow.py
@@ -0,0 +1,76 @@
+# Import necessary libraries and modules
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from src.lab import load_data, perform_eda, data_preprocessing, build_save_model, load_model_elbow
+
+from airflow import configuration as conf
+
+# Enable pickle support for XCom, allowing data to be passed between tasks
+conf.set('core', 'enable_xcom_pickling', 'True')
+
+# Define default arguments for your DAG
+default_args = {
+    'owner': 'your_name',
+    'start_date': datetime(2025, 1, 15),
+    'retries': 0, # Number of retries in case of task failure
+    'retry_delay': timedelta(minutes=5), # Delay before retries
+}
+
+# Create a DAG instance named 'Airflow_Lab1' with the defined default arguments
+dag = DAG(
+    'Airflow_Lab1',
+    default_args=default_args,
+    description='Dag example for Lab 1 of Airflow series',
+    schedule_interval=None,  # Set the schedule interval or use None for manual triggering
+    catchup=False,
+)
+
+# Define PythonOperators for each function
+
+# Task to load data, calls the 'load_data' Python function
+load_data_task = PythonOperator(
+    task_id='load_data_task',
+    python_callable=load_data,
+    dag=dag,
+)
+
+# NEW TASK: Perform EDA on loaded data
+perform_eda_task = PythonOperator(
+    task_id='perform_eda_task',
+    python_callable=perform_eda,
+    op_args=[load_data_task.output],
+    dag=dag,
+)
+
+# Task to perform data preprocessing, now depends on 'perform_eda_task'
+data_preprocessing_task = PythonOperator(
+    task_id='data_preprocessing_task',
+    python_callable=data_preprocessing,
+    op_args=[perform_eda_task.output],
+    dag=dag,
+)
+
+# Task to build and save a model, depends on 'data_preprocessing_task'
+build_save_model_task = PythonOperator(
+    task_id='build_save_model_task',
+    python_callable=build_save_model,
+    op_args=[data_preprocessing_task.output, "model.sav"],
+    provide_context=True,
+    dag=dag,
+)
+
+# Task to load a model using the 'load_model_elbow' function, depends on 'build_save_model_task'
+load_model_task = PythonOperator(
+    task_id='load_model_task',
+    python_callable=load_model_elbow,
+    op_args=["model.sav", build_save_model_task.output],
+    dag=dag,
+)
+
+# Set task dependencies - NOW WITH 5 TASKS
+load_data_task >> perform_eda_task >> data_preprocessing_task >> build_save_model_task >> load_model_task
+
+# If this script is run directly, allow command-line interaction with the DAG
+if __name__ == "__main__":
+    dag.cli()
